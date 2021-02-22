60a9d8fd2c04f747286cf15b3de8ab99e855465f
Janet Karpenske <janetkarpenske@gmail.com>
2021-02-22 12:06:22 -0800

    Updates

---
+++ precache-manifest.efce6bfb030d1e9eeca8e293d84348cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ceec3cb1794d70199afc7ae2e3b3779",
+    "url": "/deploy-proj/index.html"
+  },
+  {
+    "revision": "f3254e29690a08f07708",
+    "url": "/deploy-proj/static/css/main.8ee7dd09.chunk.css"
+  },
+  {
+    "revision": "f3c47c4f844fcb00d8c3",
+    "url": "/deploy-proj/static/js/2.3a654af4.chunk.js"
+  },
+  {
+    "revision": "27d0a587a686edf15155",
+    "url": "/deploy-proj/static/js/3.507cacfa.chunk.js"
+  },
+  {
+    "revision": "f3254e29690a08f07708",
+    "url": "/deploy-proj/static/js/main.de2895bf.chunk.js"
+  },
+  {
+    "revision": "99b9f395e8fda4b002af",
+    "url": "/deploy-proj/static/js/runtime-main.66352b28.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/deploy-proj/static/media/logo.06e73328.svg"
+  }
+]);
